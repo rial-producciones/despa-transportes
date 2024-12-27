ce426a83d851c86d31bc328440af1b20eda0f10d
agutierrez <agutierrez@tecnoaccion.com.ar>
2024-12-27 11:45:37 -0300

    Quito funcion en js

--- js/app.js
+++ js/app.js
@@ -20,31 +20,3 @@ quitMenu.addEventListener("click", (e) => {
   e.preventDefault();
   showMenu();
 });
-
-document.addEventListener("DOMContentLoaded", () => {
-  const links = document.querySelectorAll('a[href^="index.html#"]'); // Selecciona los enlaces que contienen "index.html#"
-
-  links.forEach((link) => {
-    link.addEventListener("click", (event) => {
-      event.preventDefault(); // Evita la navegación predeterminada
-      const targetId = link.getAttribute("href").split("#")[1]; // Obtén el ID del fragmento
-      const targetElement = document.getElementById(targetId);
-
-      if (targetElement) {
-        // Actualiza la URL sin recargar la página
-        history.pushState(null, "", `/${targetId}`);
-        // Desplázate al elemento objetivo
-        targetElement.scrollIntoView({ behavior: "smooth" });
-      }
-    });
-  });
-
-  // Maneja el retroceso en el historial
-  window.addEventListener("popstate", () => {
-    const path = window.location.pathname.substring(1); // Obtén la ruta sin la barra inicial
-    const targetElement = document.getElementById(path);
-    if (targetElement) {
-      targetElement.scrollIntoView({ behavior: "smooth" });
-    }
-  });
-});
